f9dc09fa2f498ffaaac63d0fe9c1ea9720ac47f1
AbeHaruhiko <h.abe@s2soft.co.jp>
2018-08-14 15:03:12 +0900

    FirebaseのAPIキーなどを.envへ。

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,13 @@ module.exports = {
   env: {
     baseUrl:
       process.env.BASE_URL ||
-      `http://${host}:${port}`
+      `http://${host}:${port}`,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID
   },
   head: {
     title: "tt1",
@@ -58,7 +64,8 @@ module.exports = {
   build: {},
   modules: [
     "@nuxtjs/axios",
-    "~/modules/typescript.js"
+    "~/modules/typescript.js",
+    "@nuxtjs/dotenv"
   ],
   axios: {},
   plugins: [
--- plugins/firestore.js
+++ plugins/firestore.js
@@ -2,10 +2,17 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 }
 
 // initializeApp が複数回呼ばれると "Firebase App named '[DEFAULT]' already exists" が発生するので
 // apps がすでにあるか判定
+console.log(config);
 const firestore = !firebase.apps.length 
   ? firebase.initializeApp(config).firestore()
   : firebase.app().firestore();
