a0e5346582087e6c046d612597121fbdde6d4da2
AbeHaruhiko <h.abe@s2soft.co.jp>
2018-08-14 15:57:21 +0900

    firestoreからの取得お試し中　

--- store/index.ts
+++ store/index.ts
@@ -1,18 +1,34 @@
+import firestore from '~/plugins/firestore'
+
 export const state = () => ({
-  people: []
+  people: [],
+  memos: []
 })
 
 export const mutations = {
   setPeople(state, people) {
     state.people = people
+  },
+  setMemos(state, memos) {
+    state.memos = memos
   }
+
 }
 
 export const actions = {
   async nuxtServerInit({ commit }, { app }) {
-    const people = await app.$axios.$get(
-      "./random-data.json"
-    )
-    commit("setPeople", people.slice(0, 10))
+    // const people = await app.$axios.$get(
+    //   "./random-data.json"
+    // )
+    // commit("setPeople", people.slice(0, 10))
+    const memosRef = firestore.collection('memos')
+    const memos = await memosRef.get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          console.log(doc.id, '=>', doc.data())
+        })
+      })
+    console.log(memos)
+    commit("setMemos", memos)
   }
 }
