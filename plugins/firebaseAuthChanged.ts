b61a18c28fc9db1b664357cacc0af1983b3f01d4
AbeHaruhiko <h.abe@s2soft.co.jp>
2018-08-23 11:30:49 +0900

    authChangedをpromiseでなくした

--- plugins/firebaseAuthChanged.ts
+++ plugins/firebaseAuthChanged.ts
@@ -1,21 +1,22 @@
 import firebase from '~/plugins/firebase'
 
-function authChanged (context) {
+// nuxt.config.jsに登録することで、ルートの Vue.js アプリケーションがインスタンス化される前に実行される
+// firebase.auth().onAuthStateChangedがイベント登録され、認証状態が変わるたびに実行され、storeの更新およびrecirectが行われる。
+function authChanged ({ store, redirect }) {
   console.log('plubing/firebaseAuthChanged/authChanged calld.(not changed yet.')
 
-  const { store } = context
-
-  return new Promise((resolve, reject) => {
+  // return new Promise((resolve, reject) => {
     firebase.auth().onAuthStateChanged((user) => {
       console.log('auth changed.')
       console.log('user =>', user)
-      // resolve(user || false)
-      // if (user) {
-        resolve(store.module.auth.commit('setUser', user))
-      // } else {
-      //   resolve()
-      // }
+      store.commit('modules/auth/setUser', user)
+      if (user) {
+        redirect('/')
+      } else {
+        redirect('/intro')
+      }      
+      // resolve()
     })
-  })
+  // })
 }
 export default authChanged
