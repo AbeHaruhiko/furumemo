e1046a6e719f9e9b43ea562c1fe1ae0fed12059c
AbeHaruhiko <h.abe@s2soft.co.jp>
2018-08-24 17:58:06 +0900

    HeaderToolbar切り出し

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,54 +1,7 @@
 <template>
   <v-app id="inspire">
     <drawer />
-    <v-toolbar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      color="blue darken-3"
-      dark
-      app
-      fixed
-    >
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="toggleDrawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">ふるめも</span>
-      </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-      <v-menu offset-y left>
-        <v-btn
-          slot="activator"
-          dark
-          icon
-        >
-          <v-avatar size="32px" tile>
-            <v-icon dark>account_circle</v-icon>
-          </v-avatar>
-        </v-btn>
-
-        <v-list>
-          <v-list-tile
-            v-for="(item, i) in account_menu_items"
-            :key="i"
-            @click="item.click_action"
-          >
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
-    </v-toolbar>
+    <header-toolbar/>
     <v-content>
       <v-container fluid fill-height>
         <v-layout justify-center>
@@ -135,46 +88,34 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import firebase from '~/plugins/firebase'
-import authChanged from '~/plugins/firebaseAuthChanged'
 import { namespace, State, Action } from 'vuex-class'
 
-const authMod = namespace('modules/auth')
-
 import Drawer from '~/components/Drawer.vue'
+import HeaderToolbar from '~/components/HaederToolbar.vue'
 
 @Component({
   props: {},
   components: {
-    Drawer
+    Drawer,
+    HeaderToolbar
   }
 })
 export default class extends Vue {
-  @authMod.Action('setUser') setUser
-  @authMod.Action('signOut') signOut
-  @Action('setDrawer') setDrawer
+  // @authMod.Action('setUser') setUser
+  // @authMod.Action('signOut') signOut
+  // @Action('setDrawer') setDrawer
 
-  @State('drawer') drawer
+  // @State('drawer') drawer
 
-  toggleDrawer() {
-    this.setDrawer(!this.drawer)
-  }
+  // toggleDrawer() {
+  //   this.setDrawer(!this.drawer)
+  // }
 
   dialog = false
 
-  items = [
-    { icon: 'home', text: 'ホーム', to: '/' },
-    { icon: 'list', text: '寄付状況', to: '/donation-status' }
-  ]
-  account_menu_items = [{ title: 'ログアウト', click_action: this.logout }]
-
-  async logout() {
-    console.log('/laytous/default/logout called.')
-    console.log('currentUser =>', firebase.auth().currentUser)
-    await this.signOut()
-    await authChanged(this.$store)
-    console.log('currentUser =>', firebase.auth().currentUser)
-    this.$router.push('/')
-  }
+  // items = [
+  //   { icon: 'home', text: 'ホーム', to: '/' },
+  //   { icon: 'list', text: '寄付状況', to: '/donation-status' }
+  // ]
 }
 </script>
