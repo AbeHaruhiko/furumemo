612d9f0e36ef8ce4fd98dd91b6f75b857d9c79be
安部 晴彦 <caliconography@abu-no-MacBook-Pro.local>
2018-08-29 00:24:29 +0900

    日付セットできた

--- components/DonationRecordDialog.vue
+++ components/DonationRecordDialog.vue
@@ -18,21 +18,50 @@
             <v-text-field
               prepend-icon="money"
               placeholder="金額"
+              mask="###,###,###,###"
             ></v-text-field>
           </v-flex>
           <v-flex xs12>
             <v-text-field
-              type="tel"
               prepend-icon="fa-gift"
               placeholder="返礼品"
-              mask="phone"
             ></v-text-field>
           </v-flex>
-          <v-flex xs12>
+          <!-- <v-flex xs12>
             <v-text-field
               prepend-icon="fa-calendar"
               placeholder="寄付日"
+              mask="####/##/##"
             ></v-text-field>
+          </v-flex> -->
+          <v-flex xs12 sm6 md4>
+            <v-menu
+              ref="menuApplicatedAt"
+              :close-on-content-click="false"
+              v-model="menuApplicatedAt"
+              :nudge-right="40"
+              :return-value.sync="applicatedAt"
+              lazy
+              transition="scale-transition"
+              offset-y
+              full-width
+              min-width="290px"
+            >
+              <v-text-field
+                slot="activator"
+                v-model="applicatedAtFormatted"
+                label="寄付日（申請日）"
+                prepend-icon="event"
+                readonly
+              ></v-text-field>
+              <v-date-picker
+                v-model="applicatedAt"
+                @input="$refs.menuApplicatedAt.save(applicatedAt)"
+                no-title
+                locale="ja"
+                :day-format="date => new Date(date).getDate()"
+              ></v-date-picker>
+            </v-menu>
           </v-flex>
           <v-flex xs12>
             <v-text-field
@@ -53,11 +82,16 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Emit } from 'nuxt-property-decorator'
+import { Vue, Component, Prop, Emit, Watch } from 'nuxt-property-decorator'
 
 @Component({})
 export default class DonationRecordDialog extends Vue {
   @Prop() show = false
+
+  menuApplicatedAt: boolean = false
+  applicatedAt: string = ''   // date picker用
+  applicatedAtFormatted: string = ''    // text field用
+
 
   // v-model="showDialog"とするためのgetter, setter
   get showDialog() {
@@ -72,6 +106,19 @@ export default class DonationRecordDialog extends Vue {
   @Emit()
   setDonationRecDialogVisiblity(value) {
     console.log('this method will emit parents set-donation-rec-dialog-visiblity')
+  }
+
+  formatDate (date): string {
+    if (!date) return ''
+
+    const [year, month, day] = date.split('-')
+    return `${year}/${month}/${day}`
+  }
+
+  // date pickerで日付が選択され、applicatedAt(v-model)が変更されたら this.formatDateを呼び出してフォーマットする
+  @Watch('applicatedAt')
+  onDateChanged(value) {
+    this.applicatedAtFormatted = this.formatDate(this.applicatedAt)
   }
 }
 </script>
